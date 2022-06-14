b54c151c7de4ebbe078431407a7edcc3b68d07b7
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-06-14 16:57:43 -0300

    projectshop

---
+++ box/script.js
@@ -0,0 +1,55 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let filterBtn = document.querySelectorAll('.filter-buttons .buttons');
+let filterItem = document.querySelectorAll('.products .box-container .box');
+
+filterBtn.forEach(button => {
+
+     button.onclick = () =>{
+        filterBtn.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        let dataFilter = button.getAttribute('data-filter');
+
+        filterItem.forEach(item => {
+
+            item.classList.remove('active');
+            item.classList.add('hide');
+
+            if(item.getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+                item.classList.remove('hide');
+                item.classList.add('active');
+            }
+        });
+     };
+});
+
+var swiper = new Swiper(".home-slider", {
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
